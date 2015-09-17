fb2bfa85a6f6b4205550099cf4028967a68eb430
Marco Pegoraro <marco.pegoraro@gmail.com>
2015-09-17 10:30:18 +0200

    test branch

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -1,24 +1,8 @@
 
-import { todosReducer, defaultValue } from 'reducers/todos-reducer';
+describe('Just a test', function() {
 
-var emptyAction = {
-    type: null
-};
-
-describe('Todos Reducer', function() {
-
-    it('should initialize', function() {
-        var state = todosReducer(undefined, emptyAction);
-        expect(state).to.deep.equal(defaultValue);
-    });
-
-});
-
-describe('setSelectedItems', function() {
-
-    it('should new id as selected', function() {
-        let selectedIds = ['1', '2', '3'];
-        setSelectedItems(selectedIds, '4', true);
+    it('should work', function() {
+        console.log('it does');
     });
 
 });
